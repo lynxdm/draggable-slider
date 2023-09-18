50e86bf422d61d28eb38fa52df5cb614774602c4
lynxdm <lynxdev32@gmail.com>
2023-09-18 17:36:47 +0000

    mobile friendly

---
+++ app.js
@@ -0,0 +1,45 @@
+let slider = document.querySelector(".slider");
+let navigation = document.querySelector(".navigation");
+let startx;
+let clicked = false;
+let counter = 1;
+
+function animate() {
+  counter++;
+  if (counter > 4) {
+    counter = 1;
+  }
+  document.getElementById("radio" + counter).checked = true;
+}
+let interval = setInterval(animate, 5000);
+
+function dragStart(e) {
+  if (navigation.contains(e.target)) return;
+  clearInterval(interval);
+  startx = e.pageX;
+  clicked = true;
+  console.log(clicked);
+}
+
+function dragging(e) {
+  if (!clicked) return;
+  e.preventDefault();
+  if (e.pageX < startx && counter < 4) {
+    counter++;
+  } else if (e.pageX > startx && counter > 1) {
+    counter--;
+  }
+  document.getElementById("radio" + counter).checked = true;
+  clicked = false;
+  interval = setInterval(animate, 5000);
+}
+
+// MOUSE EVENTS
+slider.addEventListener("mousedown", dragStart);
+
+slider.addEventListener("mousemove", dragging);
+
+// TOUCH EVENTS
+slider.addEventListener("touchstart", dragStart);
+
+slider.addEventListener("touchmove", dragging);
