2de4000f31c90b8eb7295bf5ddf80dee8076af46
lynxdm <lynxdev32@gmail.com>
2023-09-20 13:48:06 +0000

    update counter with manual navigation

--- app.js
+++ app.js
@@ -21,43 +21,60 @@ let interval = setInterval(animate, 5000);
 // *******EVENT LISTENERS******
 // MOUSE EVENTS
 slider.addEventListener("mousedown", dragStart);
-
 slider.addEventListener("mousemove", dragging);
 
 // TOUCH EVENTS
 slider.addEventListener("touchstart", dragStart);
-
 slider.addEventListener("touchmove", dragging);
 
 // ******CALLBACK FUNCTIONS******
 function dragStart(e) {
-  if (navigation.contains(e.target)) return;
-  e.preventDefault();
+  // handling manual navigation
+  if (navigation.contains(e.target)) {
+    // pause animation to avoid clashes
+    clearInterval(interval);
+
+    // update counter with manual btn id
+    counter = parseInt(e.target.id);
+
+    // resume animation
+    interval = setInterval(animate, 5000);
+
+    return; // to avoid mistaking manual navigation for drag intent
+  }
+  e.preventDefault(); // preventing touchscreen scroll defaults
+
+  // set initialX according to event type
   if (e.type == "touchstart") {
     initialX = e.touches[0].clientX;
   } else {
     initialX = e.clientX;
   }
-  clicked = true;
-  clearInterval(interval);
+
+  clicked = true; // to know that the user wants to drag
+  clearInterval(interval); // stop interval
 }
 
 function dragging(e) {
-  if (!clicked) return;
+  if (!clicked) return; // run only if dragStart ran
+
+  // set finalX according to event type
+  let finalX;
   if (e.type == "touchmove") {
-    if (e.touches[0].clientX < initialX && counter < 4) {
-      counter++;
-    } else if (e.touches[0].clientX > initialX && counter > 1) {
-      counter--;
-    }
+    finalX = e.touches[0].clientX;
   } else {
-    if (e.clientX < initialX && counter < 4) {
-      counter++;
-    } else if (e.clientX > initialX && counter > 1) {
-      counter--;
-    }
+    finalX = e.clientX;
+  }
+
+  // check in what direction the user goes
+  if (finalX < initialX && counter < 4) {
+    counter++;
+  } else if (finalX > initialX && counter > 1) {
+    counter--;
   }
   document.getElementById("radio" + counter).checked = true;
+
+  // resetting values and recalling interval
   clicked = false;
   interval = setInterval(animate, 5000);
 }
